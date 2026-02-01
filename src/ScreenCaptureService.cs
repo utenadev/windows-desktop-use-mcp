405f95b1c80d528036074e83b4ffc4480c64e319
utenadev <utena.cross@gmail.com>
2026-02-01 09:50:38 +0900

    Fix Channel class name collision with fully qualified name

--- src/ScreenCaptureService.cs
+++ src/ScreenCaptureService.cs
@@ -103,6 +103,6 @@ public class StreamSession {
     public Channel<string> Channel { get; }
     
     public StreamSession() {
-        Channel = Channel.CreateUnbounded<string>();
+        Channel = System.Threading.Channels.Channel.CreateUnbounded<string>();
     }
 }
